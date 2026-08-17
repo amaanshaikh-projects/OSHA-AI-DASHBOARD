a877fad9d38e1d6cd0283314ab9fdebcdcda74de
Amaan Zubair Shaikh <amaanshaikh.contact@gmail.com>
2026-08-17 12:52:56 +0530

    Delete frontend directory

--- frontend/debug_acorn.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const acorn = require('acorn');
-const code = fs.readFileSync('js/app.js', 'utf8');
-
-try {
-    acorn.parse(code, { ecmaVersion: 2022, sourceType: 'module' });
-} catch (e) {
-    console.log(e);
-}
--- frontend/test.js
+++
@@ -1,7 +0,0 @@
-const acorn = require('acorn');
-try {
-  acorn.parse("document.addEventListener('DOMContentLoaded', () => {\n});", { ecmaVersion: 2022, sourceType: 'module' });
-  console.log('Passed');
-} catch(e) {
-  console.log(e);
-}
